3bd7efd56a3b4a532a180410c3c29c576ce58e8f
burak <burak.alparslan@popile.com>
2023-10-16 16:58:43 +0300

    updated config

--- handler.ts
+++ handler.ts
@@ -1,24 +1,17 @@
-import { APIGatewayEvent, Context } from 'aws-lambda';
-import axios from 'axios';
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
 
-export async function hello(event: APIGatewayEvent, context: Context) {
-  try {
-    const response = await axios.get('https://jsonplaceholder.typicode.com/todos/1');
-    console.log(response.data);
-    return {
-      statusCode: 200,
-      body: JSON.stringify({
-        message: 'Request was successful!',
-        data: response.data,
-      }),
-    };
-  } catch (error) {
-    console.error(error);
-    return {
-      statusCode: 500,
-      body: JSON.stringify({
-        message: 'An error occurred!',
-      }),
-    };
-  }
-}
+export const hello = async (
+  event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: "Go Serverless v3.0! Your function executed successfully!",
+        input: event,
+      },
+      null,
+      2
+    ),
+  };
+};
