f9a861422b878faab866274b046b4e9220e06992
burak <burak.alparslan@popile.com>
2023-10-16 16:44:42 +0300

    added axios

--- handler.ts
+++ handler.ts
@@ -1,9 +1,24 @@
-export async function hello(event, context) {
+import { APIGatewayEvent, Context } from 'aws-lambda';
+import axios from 'axios';
+
+export async function hello(event: APIGatewayEvent, context: Context) {
+  try {
+    const response = await axios.get('https://jsonplaceholder.typicode.com/todos/1');
+    console.log(response.data);
     return {
       statusCode: 200,
       body: JSON.stringify({
-        message: 'Hello World!',
+        message: 'Request was successful!',
+        data: response.data,
+      }),
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        message: 'An error occurred!',
       }),
     };
   }
-  
+}
