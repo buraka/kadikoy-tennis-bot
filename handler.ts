c5e7c7ed9b269310129b8dcc2178e7b525ca3dcd
burak <burak.alparslan@popile.com>
2023-10-18 20:54:53 +0300

    added try catch

--- handler.ts
+++ handler.ts
@@ -2,27 +2,38 @@ import { ScheduledEvent } from 'aws-lambda';
 import { checkCourts, findTodaysAvailableSlots } from './tennisCourts';
 
 export async function scheduledFunction(event: ScheduledEvent) {
-  console.log('Scheduled event:', JSON.stringify(event, null, 2));
+  try {
+    console.log('Scheduled event:', JSON.stringify(event, null, 2));
 
-  await checkCourts();
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'done!',
-    }),
-  };
+    await checkCourts();
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: 'done!',
+      }),
+    };
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+
 }
 
 export async function morningFunction(event: ScheduledEvent) {
-  console.log('Scheduled event:', JSON.stringify(event, null, 2));
+  try {
+    console.log('Scheduled event:', JSON.stringify(event, null, 2));
 
-  await findTodaysAvailableSlots();
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'done!',
-    }),
-  };
+    await findTodaysAvailableSlots();
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: 'done!',
+      }),
+    };
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
 }
 
 // @ts-ignore
