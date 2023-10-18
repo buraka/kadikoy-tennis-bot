ac09f945a85f6993c162d06eac065853fb473ea6
burak <burak.alparslan@popile.com>
2023-10-18 13:21:24 +0300

    added morning script

--- handler.ts
+++ handler.ts
@@ -1,18 +1,19 @@
 import { ScheduledEvent } from 'aws-lambda';
-import { checkCourts } from './tennisCourts';
+import { checkCourts, findTodaysAvailableSlots } from './tennisCourts';
 
 export async function scheduledFunction(event: ScheduledEvent) {
   console.log('Scheduled event:', JSON.stringify(event, null, 2));
 
   await checkCourts();
-
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Function executed successfully!',
-    }),
-  };
 }
 
+export async function morningFunction(event: ScheduledEvent) {
+  console.log('Scheduled event:', JSON.stringify(event, null, 2));
+
+  await findTodaysAvailableSlots();
+}
+
+
+
 // @ts-ignore
-// scheduledFunction();
+// morningFunction();
--- tennisCourts.ts
+++ tennisCourts.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import * as moment from "moment";
+// import moment from "moment";
 import connect from './db'
 import court from './models/court';
 import { sendTelegramMessage } from './telegram';
